54c9c77841303030a27a2d544de7dc97d9d01dda
Paul-Y5 <paulo.c50@ua.pt>
2025-04-15 17:15:18 +0100

    entidades campo

---
+++ lib/models/campo_priv.dart
@@ -0,0 +1,5 @@
+import 'package:sports_link/models/campo.dart';
+
+class CampoPriv extends Campo{
+
+}
---
+++ lib/models/campo_pub.dart
@@ -0,0 +1,5 @@
+import 'package:sports_link/models/campo.dart';
+
+class CampoPub extends Campo{
+
+}
